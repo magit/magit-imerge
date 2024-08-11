09727781f9903ccb018e37c9a5e593d0fd760398
Kyle Meyer <kyle@kyleam.com>
2024-08-11 14:56:05 -0400

    Require latest Magit release

--- magit-imerge.el
+++ magit-imerge.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/magit/magit-imerge
 ;; Keywords: vc, tools
 ;; Version: 1.2.0
-;; Package-Requires: ((emacs "25.1") (magit "3.0.0"))
+;; Package-Requires: ((emacs "26.1") (magit "4.0.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
